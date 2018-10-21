1f2f0d64bfcaea852c48e2dcfac1342a26bfd49e
Igor Djukic <igordjukic0@gmail.com>
2018-10-21 21:14:17 +0200

    list of sites can only get image

--- imports/api/sites/server/publications.js
+++ imports/api/sites/server/publications.js
@@ -5,14 +5,12 @@ import { Sites } from '../sites.js';
 Meteor.publish('all.sites', function publishAllSites() {
     return Sites.find({}, {
         fields: {
-            name: 1,
-            address: 1,
             image: 1
         }
     })
 })
 
-Meteor.publish('site', function publishAllSites(siteId) {
+Meteor.publish('site', function publishSite(siteId) {
     return Sites.find({ _id: siteId }, {
         fields: {
             name: 1,
