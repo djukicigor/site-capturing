dbd038b60f4c2f9b7aa863948d36f95a70e63277
Igor Djukic <igordjukic0@gmail.com>
2018-10-21 13:06:20 +0200

    added capture and sites components to home page

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -3,23 +3,27 @@ import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 
 // Import needed templates
 import '../../ui/layouts/body/body.js';
+import { Sites } from '../../api/sites/sites.js';
 
 // Set up all routes in the app
 FlowRouter.route('/', {
   name: 'App.home',
-  action() {
-    this.render('App_body', 'App_home');
+  action(params, qs, sites) {
+    this.render('App_body', 'App_home', { sites });
   },
   waitOn() {
-    return import('../../ui/pages/home/home.js')
+    return [import('../../ui/pages/home/home.js'), Meteor.subscribe('all.sites')]
+  },
+  data() {
+    return Sites.find();
   }
 });
 
-FlowRouter.notFound = {
+FlowRouter.route('*', {
   action() {
     BlazeLayout.render('App_body', { main: 'App_notFound' });
   },
   waitOn() {
     return import('../../ui/pages/not-found/not-found.js');
   }
-};
+});
--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -1,1 +1,3 @@
 import './home.html';
+import '../../components/capture/capture.js';
+import '../../components/sites/sites.js';
