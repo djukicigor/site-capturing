54a226d6825b1e7e2fcaeab06781a97e78671193
Igor Djukic <igordjukic0@gmail.com>
2018-10-21 21:02:32 +0200

    wait for submited text and call method

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -1,3 +1,24 @@
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
+
 import './home.html';
 import '../../components/capture/capture.js';
 import '../../components/sites/sites.js';
+
+Template.App_home.helpers({
+    submitSite: () => {
+        return {
+            onSubmit(val) {
+                Meteor.call('takeScreenshot', { address: val }, (err, data) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        (async () => {
+                            const siteId = await data;
+                            FlowRouter.go('App.site', { id: siteId })
+                        })();
+                    }
+                })
+            }
+        }
+    }
+});
