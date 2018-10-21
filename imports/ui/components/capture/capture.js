b0ccc71ca54aab8dc03e358ea5cb77d0dd0bd414
Igor Djukic <igordjukic0@gmail.com>
2018-10-21 21:01:49 +0200

    get typed address

--- imports/ui/components/capture/capture.js
+++ imports/ui/components/capture/capture.js
@@ -1,1 +1,11 @@
 import './capture.html';
+
+Template.App_capture.events({
+    'submit #js-capture': function(event, template) {
+        event.preventDefault();
+        let address = event.currentTarget[0].value;
+        address = address.replace(/(^\w+:|^)\/\//, '')
+        address = address.replace('/', '');
+        template.data.onSubmit(address)
+    }
+});
